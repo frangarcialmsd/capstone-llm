034d03f05e99cf24905951bdcff63ba51e9eccb7
Francisco Ambrosio Garcia <francisco.ambrosiogarcia@kuleuven.be>
2026-09-10 13:02:14 +0000

    Read questions and answers and join them based on question_id, also add sample question and answers jsons

--- src/capstonellm/tasks/clean.py
+++ src/capstonellm/tasks/clean.py
@@ -1,13 +1,52 @@
 import argparse
 import logging
+
 from pyspark.sql import SparkSession
+
 from capstonellm.common.catalog import llm_bucket
 from capstonellm.common.spark import ClosableSparkSession
 
 logger = logging.getLogger(__name__)
 
 def clean(spark: SparkSession, environment: str, tag: str):
-    pass
+    input_prefix = (
+        f"s3a://{llm_bucket}/input/{tag}"
+        if environment != "local"
+        else "."
+    )
+
+    questions = (
+        spark.read
+        .option("multiLine", True)
+        .json(f"{input_prefix}/questions.json")
+        .selectExpr("explode(items) as question")
+        .select("question.*")
+    )
+
+    answers = (
+        spark.read
+        .option("multiLine", True)
+        .json(f"{input_prefix}/answers.json")
+        .selectExpr("explode(items) as answer")
+        .select("answer.*")
+    )
+
+    cleaned = (
+        questions.join(answers, on="question_id", how="inner")
+        .select(
+            "question_id",
+            questions.title.alias("title"),
+            questions.body.alias("question"),
+            questions.link.alias("link"),
+            answers.answer_id.alias("answer_id"),
+            answers.body.alias("answer"),
+        )
+    )
+
+    cleaned.show(5, truncate=False,st)
+    print("Rows:", cleaned.count())
+
+    return cleaned
 
 def main():
     parser = argparse.ArgumentParser(description="capstone_llm")
